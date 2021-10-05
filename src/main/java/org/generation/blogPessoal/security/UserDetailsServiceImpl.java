14f3caceae6146555233a369bcd784f798e945c1
João P. Cruz <jpcgomes0@gmail.com>
2021-10-05 13:40:23 -0300

    wip

--- src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceDetailsImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
